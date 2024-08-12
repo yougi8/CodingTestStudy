b065fb762958308c769396b7953238726f77359e
yougi8 <yougi8@naver.com>
2024-08-12 11:46:57 +0900

    백준 - 쉬운 최단거리 (개선)

--- Baekjoon/쉬운최단거리.py
+++ Baekjoon/쉬운최단거리.py
@@ -47,6 +47,54 @@ for i in range(n):
             print(board[i][j], end=' ')
     print()
 
+## 시간 약간 개선 코드
+# import sys
+# from collections import deque
+# input = sys.stdin.readline
+#
+# n, m = map(int, input().split())
+# board = []
+# for i in range(n):
+#     board.append(list(map(int, input().split())))
+# visited = [[-1]*m for _ in range(n)]
+# start = [-1,-1]
+# for i in range(n):
+#     for j in range(m):
+#         if board[i][j]==2:
+#             start[0] = i
+#             start[1] = j
+#             break
+#     if start[0]==i and start[1]==j:
+#         break
+#
+# def bfs(x,y):
+#     dx, dy = [-1,1,0,0], [0,0,-1,1]
+#     visited[x][y] = 0
+#     q = deque()
+#     q.append((x,y))
+#
+#     while q:
+#         x,y = q.popleft()
+#
+#         for i in range(4):
+#             nx, ny = x+dx[i], y+dy[i]
+#
+#             if 0<=nx<n and 0<=ny<m and visited[nx][ny]==-1:
+#                 if board[nx][ny]==0:
+#                     visited[nx][ny]=0
+#                 else:
+#                     q.append((nx,ny))
+#                     visited[nx][ny] = visited[x][y] + 1
+#
+# bfs(start[0],start[1])
+# for i in range(n):
+#     for j in range(m):
+#         if board[i][j]==0:
+#             print(0,end=' ')
+#         else:
+#             print(visited[i][j], end=' ')
+#     print()
+
 ## 예제 입력 1
 # 15 15
 # 2 1 1 1 1 1 1 1 1 1 1 1 1 1 1
