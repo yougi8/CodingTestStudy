e996044b357286808ec99f1b61b89b8523be9237
yougi8 <yougi8@naver.com>
2024-10-04 14:24:38 +0900

    프로그래머스 - 가장 큰 정사각형 찾기

---
+++ 프로그래머스/가장큰정사각형.py
@@ -0,0 +1,15 @@
+## 프로그래머스 Lv2. 가장 큰 정사각형 찾기
+def solution(board):
+    answer = 0
+
+    n = len(board)
+    m = len(board[0])
+
+    for i in range(1, n):
+        for j in range(1, m):
+            if board[i][j] == 1:
+                board[i][j] = min(board[i - 1][j - 1], board[i - 1][j], board[i][j - 1]) + 1
+
+    for i in range(n):
+        answer = max(answer, max(board[i]))
+    return answer ** 2
