544422191b25ae404e3e7b3733fa1a1d89a8702c
yougi8 <yougi8@naver.com>
2024-10-21 15:01:23 +0900

    (복습) 프로그래머스 - 리코쳇 로봇

--- 프로그래머스/리코쳇로봇.py
+++ 프로그래머스/리코쳇로봇.py
@@ -1,46 +1,49 @@
 # ## 프로그래머스 - 169199 리코쳇 로봇
-# def solution(board):
-#     answer = 0
-#
-#     rx, ry = 0, 0  # 말의 위치
-#     gx, gy = 0, 0  # 목표지점의 위치
-#
-#     for i in range(len(board)):
-#         for j in range(len(board[0])):
-#             if board[i][j] == 'R':
-#                 rx, ry = i, j
-#             elif board[i][j] == 'G':
-#                 gx, gy = i, j
-#
-#     dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
-#     visited = [[0] * len(board[0]) for i in range(len(board))]
-#     visited[rx][ry] = 1
-#
-#     def dfs(x, y, answer):
-#         if x == gx and y == gy:
-#             return
-#
-#         for i in range(4):
-#             nx, ny = x + dx[i], y + dy[i]
-#             if 0 <= nx < len(board) and 0 <= ny < len(board[0]) and visited[nx][ny] == 0:
-#                 if nx == 0 or ny == 0:
-#                     visited[nx][ny] = 1
-#                     answer += 1
-#                     dfs(nx, ny, answer)
-#         return answer
-#
-#     # 경우의 수가 만들어지는지 판단
-#     isPossible = 0
-#     if gx == 0 or gy == 0:
-#         isPossible = 1
-#     for i in range(4):
-#         if 0 <= gx + dx[i] < len(board) and 0 <= gy + dy[i] < len(board[0]):
-#             if board[gx + dx[i]][gy + dy[i]] == 'D':
-#                 isPossible = 1
-#                 break
-#
-#     if isPossible == 1:
-#         answer = dfs(rx, ry, answer)
-#         return answer
-#     else:
-#         return -1
+from collections import deque
+
+def solution(board):
+    n = len(board)
+    m = len(board[0])
+    start = [0, 0]
+    goal = [0, 0]
+
+    ## step1. 시작 지점 기록
+    for i in range(n):
+        for j in range(m):
+            if board[i][j] == 'R':
+                start = [i, j]
+                break
+
+    ## step2. bfs로 미끄러지기
+    visited = [[False] * m for _ in range(n)]
+    q = deque()
+    q.append((start[0], start[1], 0))
+    dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+    visited[start[0]][start[1]] = True
+
+    while q:
+        x, y, value = q.popleft()
+
+        if board[x][y] == 'G':
+            return value
+
+        for i in range(4):
+            nx, ny = x + dx[i], y + dy[i]
+
+            # 장애물을 마주치기 전까지 한 방향으로 미끄러지기
+            while 0 <= nx < n and 0 <= ny < m and board[nx][ny] != 'D':
+                nx += dx[i]
+                ny += dy[i]
+
+            # 장애물/보드 끝에 닿아서 while문을 빠져나왔으므로 해당 좌표로는 갈 수 없기에 이전 좌표로 돌아가기
+            nx -= dx[i]
+            ny -= dy[i]
+
+            if visited[nx][ny] == False:
+                q.append((nx, ny, value + 1))
+                visited[nx][ny] = True
+
+    return -1
+
+## 문제 풀이
+# 뽀인트 : bfs로 미끄러지는거 생각해내기!
