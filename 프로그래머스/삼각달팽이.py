ea10943b7e1d845e3a7f654552f41e3d406e6137
yougi8 <yougi8@naver.com>
2024-10-25 15:32:12 +0900

    프로그래머스 - 삼각 달팽이 (js)

--- 프로그래머스/삼각달팽이.py
+++ 프로그래머스/삼각달팽이.py
@@ -26,3 +26,30 @@ def solution(n):
 
 ## 문제 풀이
 # 아래-오른쪽-위 이 패턴을 반복한다는 것을 이용해서 그 순서대로 값을 할당해주면 된다!
+
+## js 코드
+# function solution(n) {
+#     var answer = [];
+#     const arr = Array.from({ length: n }, () => new Array(n).fill(0));
+#     const direction = [[1,0],[0,1],[-1,-1]]
+#     let x = -1
+#     let y = 0
+#     let value = 1
+#
+#     for (i=0; i<n; i++){
+#         for (j=i; j<n; j++){
+#             x += direction[i%3][0]
+#             y += direction[i%3][1]
+#
+#             arr[x][y] = value
+#             value += 1
+#         }
+#     }
+#
+#     for (i=0; i<n; i++){
+#         for (j=0;j<i+1;j++){
+#             answer.push(arr[i][j])
+#         }
+#     }
+#     return answer;
+# }
